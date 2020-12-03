266451f8b8f7a8c478b07866750d85afcc54569e
Marcelo.Rodrigues <Marcelo.Rodrigues@prodest.es.gov.br>
2020-12-03 15:30:57 -0300

    sleep test

---
+++ API/Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet("{time}/[action]")]
+        public void Sleep(int time)
+        {
+            Thread.Sleep(time);
+        }
+    }
+}
