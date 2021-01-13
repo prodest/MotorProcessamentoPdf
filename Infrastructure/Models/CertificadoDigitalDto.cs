8063de13e16e0ddfdea14421e3c75462021150e7
Marcelo.Rodrigues <Marcelo.Rodrigues@prodest.es.gov.br>
2021-01-13 15:35:38 -0300

    Ajuste nas informacoes do certificado que sera retornado

--- Infrastructure/Models/CertificadoDigitalDto.cs
+++ Infrastructure/Models/CertificadoDigitalDto.cs
@@ -7,9 +7,6 @@ namespace Infrastructure.Models
         public DateTime ValidoAPartir { get; set; }
         public DateTime ValidoAte { get; set; }
         public DateTime Pkcs7SignDate { get; set; }
-        public bool IcpBrasil { get; set; }
-        public bool CadeiaValida { get; set; }
-        public bool PeriodoValido { get; set; }
         public PessoaFisicaDto PessoaFisica { get; set; }
         public PessoaJuridicaDto PessoaJuridica { get; set; }
     }
